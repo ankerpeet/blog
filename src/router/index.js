64840aa535b7b6fc5a108c5b7834fd0689d88c18
Anker Peet <ankerpeet@gmail.com>
2017-08-21 15:23:10 -0600

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import Products from '@/components/Products'
+import Product from '@/components/Product'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    { path: '/', name: 'Home', component: Home },
+    { path: '/products', name: 'Products', component: Products },
+    { path: '/products/:productId', name: 'Product', component: Product }
+  ]
+})
+
+//export default routes
